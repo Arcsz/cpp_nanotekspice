7e74af12144836c20e58f4617afc170e07deb8d5
David Zeng <david.zeng@epitech.eu>
2017-02-07 02:17:17 +0100

    Small modif in Component.cpp

--- lib/src/Component.cpp
+++ lib/src/Component.cpp
@@ -11,25 +11,25 @@
 #include "Component.hpp"
 
 const std::map<std::string, nts::Component::mkComp> nts::Component::_funcMap = {
-  {"Input", &nts::Component::newComp<Input>},
-  {"Clock", &nts::Component::newComp<Clock>},
+  {"Input",  &nts::Component::newComp<Input>},
+  {"Clock",  &nts::Component::newComp<Clock>},
   {"Output", &nts::Component::newComp<Output>},
-  {"True", &nts::Component::newComp<True>},
-  {"False", &nts::Component::newComp<False>},
-  {"4081", &nts::Component::newComp<AND4081>},
-  {"4071", &nts::Component::newComp<OR4071>},
-  {"4011", &nts::Component::newComp<NAND4011>},
-  {"4001", &nts::Component::newComp<NOR4001>},
-  {"4030", &nts::Component::newComp<XOR4030>},
-  {"4069", &nts::Component::newComp<INVERSE4069>},
-  {"4013", &nts::Component::newComp<FLIPFLOP4013>},
-  {"4008", &nts::Component::newComp<adder4008>},
-  {"4514", &nts::Component::newComp<decoder4514>},
-  {"4040", &nts::Component::newComp<counter4040>},
-  {"4801", &nts::Component::newComp<rand4801>},
-  {"2716", &nts::Component::newComp<rdonly2716>},
-  {"4094", &nts::Component::newComp<register4094>},
-  {"4017", &nts::Component::newComp<Jdecade4017>},
+  {"True",   &nts::Component::newComp<True>},
+  {"False",  &nts::Component::newComp<False>},
+  {"4081",   &nts::Component::newComp<AND4081>},
+  {"4071",   &nts::Component::newComp<OR4071>},
+  {"4011",   &nts::Component::newComp<NAND4011>},
+  {"4001",   &nts::Component::newComp<NOR4001>},
+  {"4030",   &nts::Component::newComp<XOR4030>},
+  {"4069",   &nts::Component::newComp<INVERSE4069>},
+  {"4013",   &nts::Component::newComp<FLIPFLOP4013>},
+  {"4008",   &nts::Component::newComp<adder4008>},
+  {"4514",   &nts::Component::newComp<decoder4514>},
+  {"4040",   &nts::Component::newComp<counter4040>},
+  {"4801",   &nts::Component::newComp<rand4801>},
+  {"2716",   &nts::Component::newComp<rdonly2716>},
+  {"4094",   &nts::Component::newComp<register4094>},
+  {"4017",   &nts::Component::newComp<Jdecade4017>},
 };
 
 
@@ -41,6 +41,10 @@ nts::Component::~Component() {
 
 nts::IComponent *nts::Component::createComponent(std::string const& type,
 						 std::string const& value) const {
+  if (!_funcMap.count(type)) {
+    return NULL;
+  }
+
   mkComp func = _funcMap.at(type);
   return (this->*func)(value);
 }
