fc112c7a89c920c0447c44650b03490bdb761718
Riamon Vincent <vincent.riamon@epitech.eu>
2017-03-01 11:11:01 +0100

    dec

--- lib/src/gates/decoder-4514.cpp
+++ lib/src/gates/decoder-4514.cpp
@@ -5,12 +5,12 @@
 // Login   <riamon_v@epitech.net>
 //
 // Started on  Wed Feb  1 11:33:54 2017 Riamon Vincent
-// Last update Wed Mar  1 10:34:15 2017 Riamon Vincent
+// Last update Wed Mar  1 11:10:14 2017 Riamon Vincent
 //
 
 #include "gates/decoder-4514.hpp"
 
-nts::decoder4514::decoder4514(Tristate val) : AComponent(CONST::C4514, val, 22) {
+nts::decoder4514::decoder4514(Tristate val) : AComponent(CONST::C4514, val, 23) {
   _outputs[11] = Tristate::UNDEFINED;
   _outputs[9] = Tristate::UNDEFINED;
   _outputs[10] = Tristate::UNDEFINED;
@@ -38,11 +38,11 @@ static int isInput(size_t pin) {
   } else if (pin == 11 || pin == 9 || pin == 10 || pin == 8 ||
 	     pin == 7 || pin == 6 || pin == 5 || pin == 4 ||
 	     pin == 18 || pin == 17 || pin == 20 || pin == 19 ||
-	     pin == 14 || pin == 13 || pin == 16 || pin == 15) {
+	     pin == 14 || pin == 13 || pin == 16 || pin == 15 || pin == 23) {
     return 0;
+  } else if (pin == 23) {
+    return 2;
   }
-  else if (pin == 23)
-    return (2);
   return -1;
 }
 
@@ -277,5 +277,5 @@ nts::Tristate nts::decoder4514::calcOutput(size_t this_pin) {
   // }
 
   // return nand_gate(_pins[firstPin].compute(), _pins[secondPin].compute());
-  return Tristate::UNDEFINED;
+  return _outputs.at(this_pin);;
 }
